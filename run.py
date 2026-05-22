71bf808303dfa90004784a12e5a06803ce8e3006
linchuanbin <1029633859@qq.com>
2026-05-22 19:05:52 +0800

    生成allure静态html报告，不自动打开

--- run.py
+++ run.py
@@ -1,16 +1,12 @@
-
 import os
-import sys
-import shutil  # 用来复制文件
-
-
+import shutil
 
 # 第一步：执行用例 + 生成allure结果
 print("===== 开始执行用例，生成 Allure 结果 =====")
 os.system("pytest testcases/test_register.py testcases/test_login.py -v --alluredir=allure-results --clean-alluredir")
 
 # ======================
-# 【我加的：自动复制 3 个文件】
+# 自动复制 3 个配置文件（保留你要的功能）
 # ======================
 print("===== 自动复制环境配置文件到 allure-results =====")
 files_to_copy = [
@@ -18,22 +14,16 @@ files_to_copy = [
     "config/categories.json",
     "config/executor.json"
 ]
-#遍历列表里面三个文件
+
 for file in files_to_copy:
-    #当前路径如果存在这个文件
     if os.path.exists(file):
-        #把三个文件复制到"allure-results/"目录里面
         shutil.copy(file, "allure-results/")
-
-        print(f" 已复制 {file}")
-
         print(f"✅ 已复制 {file}")
 
-
 # ======================
-# 你原来的打开报告
+# 【关键】只生成静态 HTML 报告，不打开、不弹窗！
 # ======================
+print("===== 生成静态 Allure HTML 报告 =====")
+os.system("allure generate allure-results -o allure-report --clean")
 
-# 第二步：直接打开 Allure 报告
-print("===== 启动 Allure 报告服务 =====")
-os.system("allure serve allure-results")
+print("\n🎉 全部完成！报告已生成在 allure-report 文件夹")
